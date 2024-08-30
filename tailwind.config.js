8eb50c0a58762c501368bf5933d7180fdac5989d
orielbaptista <orielbaptista1710@gmail.com>
2024-08-30 16:01:23 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
